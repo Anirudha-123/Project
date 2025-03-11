e3636973f32ad6bf89f0a935dac2ff886341270a
Anirudha-123 <162452099+Anirudha-123@users.noreply.github.com>
2025-03-12 01:08:48 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch("https://project-backend-8ik1.onrender.com");
+        const response = await fetch("https://project-backend-8ik1.onrender.com/api/products");
         const data = await response.json();
         setProducts(data);
       } catch (error) {
