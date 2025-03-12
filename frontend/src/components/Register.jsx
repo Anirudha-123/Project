689c37b3b8d30dec91c8c2dd0f3df496432071d3
Anirudha-123 <162452099+Anirudha-123@users.noreply.github.com>
2025-03-12 20:21:36 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -26,7 +26,7 @@ const Register = () => {
       return;
     }
     try {
-      const response = await fetch("https://project-frontend-oclo.onrender.com/api/auth/register", {
+      const response = await fetch("https://project-backend-8ik1.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
