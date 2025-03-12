fdf0edc0060be8c73fa7bf268c91e90a5573bcfd
Anirudha-123 <162452099+Anirudha-123@users.noreply.github.com>
2025-03-12 19:57:39 +0530

    Update AdminAddProducts.jsx

--- frontend/src/components/AdminAddProducts.jsx
+++ frontend/src/components/AdminAddProducts.jsx
@@ -121,7 +121,7 @@ const AdminAddProducts = () => {
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("https://project-backend-8ik1.onrender.com", form, {
+      await axios.post("https://project-backend-8ik1.onrender.com/api/products", form, {
         headers: { Authorization: `Bearer ${authData.token}` },
       });
       setMessage("Product added successfully!");
