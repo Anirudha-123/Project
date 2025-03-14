ed07a1a51460744fa8b5bc3000946f9d252c02fc
Anirudha-123 <162452099+Anirudha-123@users.noreply.github.com>
2025-03-14 18:48:35 +0530

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -18,6 +18,54 @@
 
 // export default router;
 
+// import express from "express";
+// import { 
+//   placeOrder, 
+//   getMyOrders, 
+//   getAllOrders, 
+//   updateOrder, 
+//   deleteOrder, 
+//   deleteUserOrder 
+// } from "../controllers/orderController.js";
+// import { authMiddleware, adminMiddleware } from "../middleware/authMiddleware.js";
+
+// const router = express.Router();
+
+// //router.post("/place", authMiddleware, placeOrder);
+// router.post("/", authMiddleware, placeOrder);
+
+// router.get("/my-orders", authMiddleware, getMyOrders);
+// router.get("/all-orders", authMiddleware, adminMiddleware, getAllOrders);
+// router.put("/update/:id", authMiddleware, adminMiddleware, updateOrder);
+// router.delete("/delete/:id", authMiddleware, adminMiddleware, deleteOrder);
+
+// // New route for users to delete orders from their history
+// router.delete("/delete-user-order/:id", authMiddleware, deleteUserOrder);
+
+// export default router;
+
+
+
+// import express from "express";
+// import { authMiddleware } from "../middleware/authMiddleware.js";
+// import {
+//   placeOrder,
+//   getMyOrders,
+//   updateOrder,
+//   deleteOrder,
+//   getAllOrders
+// } from "../controllers/orderController.js";
+
+// const router = express.Router();
+
+// router.post("/", authMiddleware, placeOrder);
+// router.get("/myorders", authMiddleware, getMyOrders);
+// router.put("/:id", authMiddleware, updateOrder);
+// router.delete("/:id", authMiddleware, deleteOrder);
+// router.get("/admin-orders", authMiddleware, getAllOrders);
+
+// export default router;
+
 import express from "express";
 import { 
   placeOrder, 
@@ -25,7 +73,8 @@ import {
   getAllOrders, 
   updateOrder, 
   deleteOrder, 
-  deleteUserOrder 
+  deleteUserOrder ,
+  searchOrderById
 } from "../controllers/orderController.js";
 import { authMiddleware, adminMiddleware } from "../middleware/authMiddleware.js";
 
@@ -41,6 +90,12 @@ router.delete("/delete/:id", authMiddleware, adminMiddleware, deleteOrder);
 
 // New route for users to delete orders from their history
 router.delete("/delete-user-order/:id", authMiddleware, deleteUserOrder);
+// Add this route for searching order by ID
+router.get("/search-order", searchOrderById);
+
+//router.get("/all-orders", authMiddleware, adminMiddleware, getAllOrders);
+
 
 export default router;
+
 
