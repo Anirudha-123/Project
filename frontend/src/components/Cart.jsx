28a592ebeb83f0500804cd8b500fca68854123cd
Anirudha-123 <162452099+Anirudha-123@users.noreply.github.com>
2025-03-14 17:43:27 +0530

    Update Cart.jsx

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -134,6 +134,7 @@ import React from "react";
 import { useCart } from "../context/CartContext";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import "./cart.css";
 
 const Cart = () => {
   const { cartItems, updateCartItemQuantity, removeCartItem } = useCart();
